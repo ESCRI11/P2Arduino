59b62f73d0725a6f58950c790c836e700cae1c16
escri11 <escri11@yahoo.es>
2017-04-27 00:15:38 +0200

    Included map function

--- src/main.ino
+++ src/main.ino
@@ -4,6 +4,8 @@ int red = 6;
 double color_red = 0;
 double color_blue = 0;
 double i = 0;
+double t_min = 10000;
+double t_max = 0;
 void setup () {
   pinMode(blue, OUTPUT);
   pinMode(red, OUTPUT);
@@ -13,10 +15,13 @@ void setup () {
 void loop () {
   i = analogRead(pinEntrada);
   i = i*5/1023*1000/10; //Digital value to degrees celsius
-  color_red = i*32;
-  color_blue = 256 - color_red;
-  digitalWrite(red, color_red);
-  digitalWrite(blue, color_blue);
+  if (i <= t_min) {t_min = i;}
+  if (i >= t_max) {t_max = i;}
+  color_red = map(i, t_min, t_max, 0, 255);
+  color_blue = 255 - color_red;
+  analogWrite(red, color_red);
+  analogWrite(blue, color_blue);
+//  Serial.print(i);
   Serial.println(color_red);
-  delay(1000);
+  //delay(1000);
 }
