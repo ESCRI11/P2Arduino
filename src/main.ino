443165c1b219b6b319184504e2be86ba12ac5b53
escri11 <escri11@yahoo.es>
2017-04-26 16:09:57 +0200

    LM35 example

--- src/main.ino
+++ src/main.ino
@@ -1,11 +1,11 @@
 int pinEntrada = 3;
-int i = 0;
+double i = 0;
 void setup () {
-  pinMode(pinEntrada, INPUT);
   Serial.begin(9600);
 }
 
 void loop () {
   i = analogRead(pinEntrada);
-  Serial.println(i); // Should be 675
+  Serial.println(i*5/1024*1000/10);
+  delay(1000);
 }
