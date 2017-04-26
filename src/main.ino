ec2cd2826fa1c2949d9688af1c0572081cb47cc6
escri11 <escri11@yahoo.es>
2017-04-26 17:53:20 +0200

    Primera versio del homework

--- src/main.ino
+++ src/main.ino
@@ -1,12 +1,22 @@
 int pinEntrada = 3;
+int blue = 5;
+int red = 6;
+double color_red = 0;
+double color_blue = 0;
 double i = 0;
 void setup () {
+  pinMode(blue, OUTPUT);
+  pinMode(red, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop () {
   i = analogRead(pinEntrada);
-  Serial.print(i*5/1023*1000/10); //Digital value to degrees celsius
-  Serial.println('C');
+  i = i*5/1023*1000/10; //Digital value to degrees celsius
+  color_red = i*32;
+  color_blue = 256 - color_red;
+  digitalWrite(red, color_red);
+  digitalWrite(blue, color_blue);
+  Serial.println(color_red);
   delay(1000);
 }
